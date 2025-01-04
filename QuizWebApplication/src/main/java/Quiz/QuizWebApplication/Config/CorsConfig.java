72482ca9e7446bb34a329a0f3fee442cdfb2e827
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-01-04 16:37:01 +0530

    changes

--- QuizWebApplication/src/main/java/Quiz/QuizWebApplication/Config/CorsConfig.java
+++ QuizWebApplication/src/main/java/Quiz/QuizWebApplication/Config/CorsConfig.java
@@ -34,7 +34,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                        .allowedOrigins("http://localhost:5173/","https://quizaki.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
